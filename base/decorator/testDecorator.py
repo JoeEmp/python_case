31e89fad7de4aa08da128cc26b8c5752999e574a
JoeEmp <hemingjie>
2020-09-27 17:30:50 +0800

    修改urlbug

--- base/decorator/testDecorator.py
+++ base/decorator/testDecorator.py
@@ -8,30 +8,34 @@
 import requests
 from enum import Enum
 
+url_env = 'debug'
+
 
 class ENV(Enum):
     debug = 'http://127.0.0.1:5000'
     pro = 'https://todo.winn.online'
 
-def url(url, env='test'):
+
+def url(url: str, env=url_env):
     def decorator(func):
         def wrapper(*args, **kwargs):
             try:
                 prefix = ENV[env].value+'/'
             except:
                 prefix = ENV['debug'].value+'/'
-            return func(prefix+url, *args, **kwargs)
+            return func(prefix+url.lstrip('/'), *args, **kwargs)
         return wrapper
     return decorator
 
 
-@url('/', env='test')
-def get_hello(url,*args,*kwargs):
+@url('/', env='debug')
+def get_hello(url, *args, **kwargs):
     r = requests.get(url)
     print(r.text)
 
+
 @url('/ContributorList')
-def get_ContributorList(url, *args,**kwargs):
+def get_ContributorList(url, *args, **kwargs):
     r = requests.get(url, data=kwargs)
     print(r.text)
 
@@ -39,5 +43,5 @@ def get_ContributorList(url, *args,**kwargs):
 if __name__ == "__main__":
     test = 456
     get_hello()
-    # get_ContributorList(test=test)
-    # get_ContributorList(name=test)
+    get_ContributorList(test=test)
+    get_ContributorList(name=test)
