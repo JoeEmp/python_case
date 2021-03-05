ac13cf37f6de213b5683a8f48d20507956c91187
JoeEmp <hemingjie>
2021-03-05 13:21:09 +0800

    [enhand]http2.0支持

--- frame/base_api.py
+++ frame/base_api.py
@@ -1,5 +1,7 @@
-import requests
-
+from requests import request, session,Response
+import logging
+from hyper.contrib import HTTP20Adapter
+import re
 api_proxies = {
     'http': 'http://0.0.0.0:8888/',
     'https': 'https://0.0.0.0:8888/'
@@ -11,9 +13,10 @@ class base_api(object):
 
     def __init__(self):
         self.last_url, self.last_response = None, None
+        self.log_str = "\nurl:{url}\nbody:{body}\nreponese{response}\n"
 
     @staticmethod
-    def is_normal(response: requests.Response, code_ranges=list()) -> bool:
+    def is_normal(response: Response, code_ranges=list()) -> bool:
         """ check http status code,you can use code_ranges assert expectation value """
         if not code_ranges:
             return response.ok
@@ -21,24 +24,34 @@ class base_api(object):
             return True
         return False
 
-    def get_api(self, url, method='post', *args, **kwargs) -> dict:
+    def get_api(self, url: str, method='post', *args, **kwargs) -> dict:
+        """version param is support http2.0 request."""
         ret = dict(status=True)
         try:
-            if 'post' == method:
-                response = requests.post(url, **kwargs)
-            elif 'get' == method:
-                response = requests.get(url, **kwargs)
-            elif 'options' == method:
-                response = requests.options(url, **kwargs)
+            s = session()
+            version = kwargs.pop('version',None)
+            if version and '2.0' == version:
+                search = r'https://.+?/' if url.startswith(
+                    'https') else r'http://.+?/'
+                start, end = re.search(search, url).span()
+                url_perfix = url[start:end-1]
+                s.mount(url_perfix, HTTP20Adapter())
+            response = s.request(method, url, **kwargs)
             if 'code_ranges' in kwargs.keys():
                 kwargs['code_ranges'] = list()
                 ret['status'] = base_api.is_normal(
                     response, code_ranges=kwargs['code_ranges'])
             try:
-                ret['body'] = response.json()
+                ret['response'] = response.json()
             except:
-                ret['body'] = response.text
+                ret['response'] = response.text
             self.last_url, self.last_response = url, response
+            for key in ['data', 'json', 'params']:
+                if key in kwargs.keys():
+                    body = kwargs[key]
+            if kwargs.get('withlog', None):
+                logging.info(self.log_str.format_map(
+                    dict(url=url, body=body, response=ret['response'])))
         except Exception as e:
             ret['status'], ret['reason'] = False, e
         return ret
@@ -60,3 +73,8 @@ class base_api(object):
         last_url,last_response分别为最近一次的请求地址和请求结果(Response)。在并发请求的时候请注意这两个属性的使用.
         """
 
+
+if __name__ == "__main__":
+    api = base_api()
+    print(api.get("http://www.baidu.com/s",version='2.0'))
+    print(api.get("http://www.baidu.com/s"))
