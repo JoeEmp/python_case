5cdff3c95ca892b18ed689a4bb64cb4eebb7fcca
JoeEmp <hemingjie>
2021-06-21 01:58:22 +0800

    [code]补充遗漏提交

--- frame/Mitmproxy/replay.py
+++ frame/Mitmproxy/replay.py
@@ -9,16 +9,13 @@ dirpath = os.path.abspath(os.path.dirname(__file__))
 
 def get_records(filename):
     records = []
-    filename = dirpath+'/'+filename
+    filename = dirpath + os.sep + filename
     with open(filename, 'r') as f:
         records = [eval(line.strip(os.linesep))
                    for line in f if not line.startswith(os.linesep)]
     for record in records:
         new_headers = {}
         for key, value in record['headers'].items():
-            # requests 不支持2.0请求头,过滤请求头
-            if ":" == key[0]:
-                continue
             new_headers[str(key)] = str(value)
         record['headers'] = new_headers
     return records
@@ -29,12 +26,17 @@ if __name__ == "__main__":
     sys.path.append(last_dirpath)
     from base_api import base_api, api_proxies
     api = base_api()
-    log_name = 'record_20210116182333.log'
+    log_name = 'record_20210621010243.log'
     for record in get_records(log_name):
-        url, headers, data = record['url'], record['headers'], record['text']
-        if 'vcount' in url:
-            s = requests.Session()
-            s.mount('https://91mjw.com', HTTP20Adapter())
-            r = s.post(url,headers=headers)
-            print(r.text)
-            # ret = api.post(url, headers=headers, data=data, proxies=api_proxies,verify=False)
+        kwargs = {
+            "url": record['url'],
+            "method": record['method'],
+            "version": record['version'],
+            "headers": record['headers']
+        }
+        if 'json' in record['headers']['content-type']:
+            kwargs['json'] = json.loads(record['text'])
+        if 'form' in record['headers']['content-type']:
+            kwargs['data'] = json.loads(record['text'])
+        r = api.get_api(**kwargs)
+        print(r)
