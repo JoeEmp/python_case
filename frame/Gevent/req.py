15843b229ea244142d2ebcc9b087f78693d153d9
JoeEmp <2057567309@qq.com>
2021-08-26 19:28:36 +0800

    优化Gevent demo

--- frame/Gevent/req.py
+++ frame/Gevent/req.py
@@ -1,30 +1,24 @@
-'''
-@Author: your name
-@Date: 2019-12-29 11:51:56
-@LastEditTime : 2019-12-29 15:00:06
-@LastEditors  : Please set LastEditors
-@Description: In User Settings Edit
-@FilePath: /Gevent/req.py
-'''
-import requests
 import random
+import time
+import requests
 import gevent
 from gevent import Timeout, monkey
-import time
-import signal
+
 monkey.patch_all()
 
-timer = Timeout(1).start()
+TIMER = Timeout(1)
+TIMER.start()
 
-def get_data(num,begin=0,**kwargs):
+
+def get_data(num, begin=0, **kwargs):
     """
-    
+
     Arguments:
         num {[int} -- 获取数量
-    
+
     Yields:
         [str] -- 用户名
-    """    
+    """
     if 'filename' not in kwargs:
         kwargs['filename'] = ''
     try:
@@ -34,43 +28,47 @@ def get_data(num,begin=0,**kwargs):
                 if i < begin:
                     continue
                 yield lines[i]
-    except IndexError as e:
+    except IndexError:
         print('文件内容不足，使用随机生成名字')
-        for i in range(len(lines),num):
+        for i in range(len(lines), num):
             yield make_name()
-    except FileNotFoundError as e:
+    except FileNotFoundError:
         for i in range(num):
             yield make_name()
 
-def make_name(len=4):
+
+def make_name(length=4):
     s = ''
     for i in range(len):
         if 0 == i:
-            s += chr(random.randrange(65,65+26))
+            s += chr(random.randrange(65, 65+26))
         else:
-            s += chr(random.randrange(97,97+26))
-    print('生成name：',s)
+            s += chr(random.randrange(97, 97+26))
+    print('生成name：', s)
     return s
 
-def make_data(num,filename):
-    with open(filename,'w') as f:
-        for i in range(num):
+
+def make_data(num, filename):
+    with open(filename, 'w') as f:
+        for _ in range(num):
             f.write(make_name+'\n')
         f.close()
 
-def req(name):
-    return requests.get('http://localhost:5000/%s'%name)
 
-def erupt_get_hello(num,**kwargs):
+def req(name):
+    return requests.get('http://localhost:5000/%s' % name)
+
+
+def erupt_get_hello(num, **kwargs):
     if 'filename' not in kwargs:
         kwargs['filename'] = ''
         begin_time = time.time()
     # 异步模式
     run_gevent_list = []
     for name in get_data(num):
-        run_gevent_list.append(gevent.spawn(req,name))
+        run_gevent_list.append(gevent.spawn(req, name))
     try:
-        gevent.joinall(run_gevent_list,timeout=timer)
+        gevent.joinall(run_gevent_list, timeout=TIMER)
     except Timeout:
         print('返回超时')
     # 同步处理查看哪一次请求返回缓慢，但是速度慢
@@ -83,6 +81,7 @@ def erupt_get_hello(num,**kwargs):
     end = time.time()
     print('单次测试时间（平均）s:', (end - begin_time) / num)
     print('累计测试时间 s:', end - begin_time)
+
 
 if __name__ == "__main__":
     erupt_get_hello(100)
--- frame/Gevent/server.py
+++ frame/Gevent/server.py
@@ -1,24 +1,18 @@
-'''
-@Author: JoeEmp
-@Date: 2019-12-29 11:53:48
-@LastEditTime : 2019-12-29 15:00:46
-@LastEditors  : Please set LastEditors
-@Description: In User Settings Edit
-@FilePath: /Gevent/server.py
-'''
-from flask import Flask
 import random
 from time import sleep
-app = Flask(__name__)
+from flask import Flask
+APP = Flask(__name__)
 
-@app.route('/<username>')
+
+@APP.route('/<username>')
 def hello_user(username='world'):
     # 模拟返回缓慢
-    i = random.randint(0,1000)
+    i = random.randint(0, 1000)
     if i > 800:
         sleep(2)
-        print('%s requests sleep 2s'%username)
-    return 'Hello, %s'%username
+        print('%s requests sleep 2s' % username)
+    return 'Hello, %s' % username
+
 
 if __name__ == "__main__":
-    app.run(debug=False,port=5000)
+    APP.run(debug=False, port=5000)
